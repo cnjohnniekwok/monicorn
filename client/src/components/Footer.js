14643f2babb2e2f638e0ebecadc43013c0a8ac35
Johnnie Kwok <cnjohnniekwok@gmail.com>
2021-04-09 21:44:09 +0800

    edit copy right

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -13,6 +13,30 @@ const Footer = () => {
 				</Row>
 				<Row>
 					<Col className="text-center copyright">
+						<section className="mb-4">
+							<a
+								className="btn btn-outline-light btn-floating m-1"
+								href="mailto:cnjohnniekwok@gmail.com"
+								role="button"
+							>
+								<i className="fab fa-google"></i>
+							</a>
+							<a
+								className="btn btn-outline-light btn-floating m-1"
+								href="https://www.linkedin.com/in/johnniekwok/"
+								target="_blank"
+								role="button"
+							>
+								<i className="fab fa-linkedin-in"></i>
+							</a>
+							<a
+								className="btn btn-outline-light btn-floating m-1"
+								href="https://github.com/cnjohnniekwok/mobar"
+								role="button"
+							>
+								<i className="fab fa-github"></i>
+							</a>
+						</section>
 						Copyright &copy; {new Date().getFullYear()} Johnnie Kwok@MoNicorn
 					</Col>
 				</Row>
